36b10463ce43580792377a4ff8c4e4c002be43ac
smv1999 <vaidhyanathan.sm@gmail.com>
2020-10-25 15:11:59 +0530

    end of Basic JS Series

--- app.js
+++ app.js
@@ -65,3 +65,130 @@ console.log(typeof name);
  
  if( age === 18)
  console.log('Perfect!')
+
+
+ // Arrays
+ const schedule = [
+     'Wake Up',
+     'Eat',
+     'Start Coding',
+     'Watch Series'
+ ];
+
+ console.log(schedule);
+ 
+ // add elements
+ schedule.push('Sleep');
+ 
+ console.log(schedule);
+
+ console.log(schedule.pop());
+
+ // unshift is used to add at the beginning and shift to remove from the beginning
+ 
+schedule.unshift("something");
+
+console.log(schedule);
+
+// return the indexOf the element
+
+console.log(schedule.indexOf('Eat'));
+
+
+// JS Objects
+
+const user = {
+    name: 'Vaidhyanathan S M',
+    age: 20,
+    married: false,
+    purchases: ['Phone', 'Computer'],
+    sayName: () => {
+        console.log(this.name);
+    }
+}
+ 
+console.log(user.purchases);
+
+
+// this function atumatically is registered in memory of the window object
+function printsomething(){ 
+    console.log("something great!");
+}
+
+// The above function can be accesed like below:
+window.printsomething();
+
+// this keyword in JS
+
+console.log(this); // returns the window object by default
+
+user.sayName();
+
+
+// For and While loops in JS
+
+
+//  for loop
+const names = [
+    'John',
+    'Peter',
+    'Harry',
+    'Bob'
+];
+
+for(name of names){
+    console.log(`Hello there ${name}`);
+}
+
+for(name of names){
+    if(name === 'Harry'){
+        console.log(name);
+        break;
+    }
+}
+
+// while loop
+
+let loading = 0;
+
+while(loading < 100){
+    console.log('Website is still loading!');
+    loading++;
+}
+
+// Accessing the HTML elements using the document object
+const text = document.querySelector(".title");
+// const changeColor = document.querySelector(".changeColor");
+
+// text.style.color = "red";
+
+// text.classList.add('change');
+
+// changeColor.addEventListener('click', () => {
+//     text.classList.toggle('change');
+// });
+
+// const userList = document.querySelectorAll(".name-list li");
+const userList = document.querySelector(".name-list li");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
+
+console.log(userList);
+
+// access each list element
+// for(eachUser of userList){
+//     eachUser.addEventListener('click', function (){
+//         this.style.color = "red";
+//         console.log(this);
+//     });
+// }
+
+addListBtn.addEventListener('click', function() {
+    const newLi = document.createElement("li");
+    const liContent = document.createTextNode(listInput.value);
+    newLi.appendChild(liContent);
+
+    userList.append(newLi);
+    listInput.value = "";
+    // console.log(listInput.value);
+});
